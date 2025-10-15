5122780bfc61ff9f6a55ce5cf3fa78e47aebf067
Niklas Büschelmann <niklas.bueschelmann@web.de>
2025-10-15 21:06:59 +0200

    Haesslicher erster code Steht

--- KO_Artemis_1/EnumerationSolver.cpp
+++ KO_Artemis_1/EnumerationSolver.cpp
@@ -2,9 +2,60 @@
 
 
 vector<int> EnumerationSolver::solve(Instance& toSolve){
-
-    // TODO: implement this and delete the following cerr + exit statement
-    cerr << "EnumerationSolver::solve not implemented yet!\n";
-    exit(EXIT_FAILURE);
+    Solution loesung = Solution(toSolve);
+    solve_helper(loesung,toSolve,0);
+    return count_solution();
 }
+
+vector<int> EnumerationSolver::count_solution()
+{
+    vector<int> sol;
+    sol.push_back(solutions_.size());
+    int count = 0;
+    int max = 0;
+    if(solutions_.size() >0)
+    {
+        count = 1;
+        max = solutions_[0].getValue();
+        for(int i = 1; i < solutions_.size(); i++)
+        {
+            if(solutions_[i].getValue() > max)
+            {
+                count = 1;
+                max = solutions_[i].getValue();
+            }
+            else if(solutions_[i].getValue() == max)
+            {
+                count++;
+            }
+        }
+    }
+    sol.push_back(count);
+    sol.push_back(max);
+    return sol;
+}
+
+
+void EnumerationSolver::solve_helper(Solution loesung, const Instance& instanz, int depth)
+{
+    {
+        if(depth == instanz.getNumberOfItems())
+        {
+            if (loesung.isFeasible())
+            {
+                solutions_.push_back(loesung);
+            }
+        }
+        else
+        {
+            Solution temp = loesung;
+            loesung.set(depth,1);
+            solve_helper(loesung,instanz,depth+1); // nehmen
+            temp.set(depth,0);
+            solve_helper(temp,instanz,depth+1); // nicht nehmen
+        }
+    }
+}
+
+vector<Solution> EnumerationSolver::solutions_ = {};
 
--- KO_Artemis_1/EnumerationSolver.hpp
+++ KO_Artemis_1/EnumerationSolver.hpp
@@ -11,8 +11,13 @@ using namespace std;
 
 // TODO: comment this class with Doxygen (and delete this comment)
 class EnumerationSolver {
-
+    static std::vector<Solution> solutions_;
 public:
+
+
+    // Todo: Funktion schneller machen, maybe call by reference anstatt Solution objekt zu Kopieren
+    static void solve_helper(Solution loesung, const Instance& instanz, int depth);
+    static vector<int> count_solution();
 
     static vector<int> solve(Instance& toSolve);
 };
