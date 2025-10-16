6436a330e9355449c0a84b346fb9daca64d3746e
Nuraxq <niklas.bueschelmann@web.de>
2025-10-16 12:16:56 +0200

    Abgabefertig

--- KO_Artemis_1/EnumerationSolver.cpp
+++ KO_Artemis_1/EnumerationSolver.cpp
@@ -27,59 +27,6 @@ void EnumerationSolver::backtrack( Solution &loesung,int depth,int max) {
         backtrack(loesung,depth+1,max);
     }
 }
-
-
-/*
-vector<int> EnumerationSolver::count_solution()
-{
-    vector<int> sol;
-    sol.push_back(solutions_.size());
-    int count = 0;
-    int max = 0;
-    if(solutions_.size() >0)
-    {
-        count = 1;
-        max = solutions_[0].getValue();
-        for(int i = 1; i < solutions_.size(); i++)
-        {
-            if(solutions_[i].getValue() > max)
-            {
-                count = 1;
-                max = solutions_[i].getValue();
-            }
-            else if(solutions_[i].getValue() == max)
-            {
-                count++;
-            }
-        }
-    }
-    sol.push_back(count);
-    sol.push_back(max);
-    return sol;
-}
-
-
-void EnumerationSolver::solve_helper(Solution loesung, const Instance& instanz, int depth)
-{
-    {
-        if(depth == instanz.getNumberOfItems())
-        {
-            if (loesung.isFeasible())
-            {
-                solutions_.push_back(loesung);
-            }
-        }
-        else
-        {
-            Solution temp = loesung;
-            loesung.set(depth,1);
-            solve_helper(loesung,instanz,depth+1); // nehmen
-            temp.set(depth,0);
-            solve_helper(temp,instanz,depth+1); // nicht nehmen
-        }
-    }
-}
-*/
 int EnumerationSolver::numberFeasible_ = 0;
 int EnumerationSolver::numberOptimal_ = 0;
 int EnumerationSolver::optimalValue_ = 0;
